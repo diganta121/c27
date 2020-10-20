fe87dca3122feb8f4a5721e2584e0a9ff8b06cfb
diganta121 <duttadiganta049@gmail.com>
2020-10-20 21:02:06 +0530

    c27

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
@@ -11,7 +13,7 @@ function preload() {
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    var canvas = createCanvas(1600,790);
     engine = Engine.create();
     world = engine.world;
 
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -58,6 +62,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    chain.display();
 
     bird.display();
     platform.display();
